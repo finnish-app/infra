3eaa56c717b51a3fb47089b1844c909b207a2803
Nícolas Varela Auler <nicolasauler@usp.br>
2024-12-06 12:16:11 -0300

    attempt: pull from fina cachix on CI (#4)

--- nixos/digitalOcean/configuration.nix
+++ nixos/digitalOcean/configuration.nix
@@ -19,11 +19,13 @@
       "https://cache.nixos.org/"
     ];
     substituters = [
+      "https://fina.cachix.org"
       "https://cuda-maintainers.cachix.org"
       "https://hyprland.cachix.org"
       "https://devenv.cachix.org"
     ];
     trusted-public-keys = [
+      "fina.cachix.org-1:Xaf+3HF5Wffl4Gtpi68Yz/wRQw0bH8tNVTlMnkLSRQc="
       "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="

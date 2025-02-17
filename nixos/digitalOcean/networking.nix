66e10c2fb53b23add1dab09338d64ace8a82748d
System administrator <nickvarauler@gmail.com>
2025-02-17 19:24:24 +0000

    change networking to bip-droplet

--- nixos/digitalOcean/networking.nix
+++ nixos/digitalOcean/networking.nix
@@ -4,9 +4,9 @@
   networking = {
     nameservers = [ "8.8.8.8"
  ];
-    defaultGateway = "164.92.80.1";
+    defaultGateway = "157.230.48.1";
     defaultGateway6 = {
-      address = "2604:a880:4:1d0::1";
+      address = "2604:a880:400:d1::1";
       interface = "eth0";
     };
     dhcpcd.enable = false;
@@ -14,28 +14,28 @@
     interfaces = {
       eth0 = {
         ipv4.addresses = [
-          { address="164.92.87.138"; prefixLength=20; }
-{ address="10.48.0.5"; prefixLength=16; }
+          { address="157.230.52.132"; prefixLength=20; }
+{ address="10.10.0.5"; prefixLength=16; }
         ];
         ipv6.addresses = [
-          { address="2604:a880:4:1d0::26a8:d000"; prefixLength=64; }
-{ address="fe80::f4a4:f0ff:fe78:e7b"; prefixLength=64; }
+          { address="2604:a880:400:d1:0:1:8f4:5001"; prefixLength=64; }
+{ address="fe80::18b6:63ff:fe55:cb6c"; prefixLength=64; }
         ];
-        ipv4.routes = [ { address = "164.92.80.1"; prefixLength = 32; } ];
-        ipv6.routes = [ { address = "2604:a880:4:1d0::1"; prefixLength = 128; } ];
+        ipv4.routes = [ { address = "157.230.48.1"; prefixLength = 32; } ];
+        ipv6.routes = [ { address = "2604:a880:400:d1::1"; prefixLength = 128; } ];
       };
             eth1 = {
         ipv4.addresses = [
-          { address="10.124.0.2"; prefixLength=20; }
+          { address="10.116.0.2"; prefixLength=20; }
         ];
         ipv6.addresses = [
-          { address="fe80::a073:71ff:fe6f:485f"; prefixLength=64; }
+          { address="fe80::4c9f:5fff:fe09:ce8c"; prefixLength=64; }
         ];
         };
     };
   };
   services.udev.extraRules = ''
-    ATTR{address}=="f6:a4:f0:78:0e:7b", NAME="eth0"
-    ATTR{address}=="a2:73:71:6f:48:5f", NAME="eth1"
+    ATTR{address}=="1a:b6:63:55:cb:6c", NAME="eth0"
+    ATTR{address}=="4e:9f:5f:09:ce:8c", NAME="eth1"
   '';
 }
